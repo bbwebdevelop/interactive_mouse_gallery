c2384659d1467559deae749f1bd6d81bddff6ab3
bbwebdevelop <bbwebdevelop@gmail.com>
2024-02-22 14:48:43 +0100

    interactive_mouse_gallery

---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const projects = [
+    {
+        name: "bbwebdevelop",
+        client: "bb",
+        description: "add description",
+        src: "2.jpg",
+        year: 2023,
+    },
+    {
+        name: "bbwebdevelop",
+        client: "bb",
+        description: "add description.",
+        src: "3.jpg",
+        year: 2023,
+    },
+    {
+        name: "bbwebdevelop",
+        client: "bb",
+        description: "add description",
+        src: "4.jpg",
+        year: 2022,
+    },
+    {
+        name: "bbwebdevelop",
+        client: "bb",
+        description: "add description",
+        src: "1.jpg",
+        year: 2022,
+    },
+    {
+        name: "bbwebdevelop",
+        client: "bb",
+        description: "add description",
+        src: "5.jpg",
+        year: 2023,
+    },
+    {
+        name: "bbwebdevelop",
+        client: "bb",
+        description: "add description",
+        src: "6.jpg",
+        year: 2022,
+    },
+    {
+        name: "bbwebdevelop",
+        client: "bb",
+        description: "add description",
+        src: "7.jpg",
+        year: 2021,
+    },
+    {
+        name: "bbwedevelop",
+        client: "bb",
+        description: "add description",
+        src: "8.jpg",
+        year: 2020,
+    }
+]
+
